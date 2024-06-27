5a2705a66d01c4e5d6c782d1c8821536e79d7028
Bweb <34779521+EdoardoDevelop@users.noreply.github.com>
2024-06-27 17:12:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 class gfonts {
     private $bc_gfonts_options;
 	public function __construct() {	
-	if(!isset(get_option( 'bc_key_gfont' ))){
+	if(!get_option( 'bc_key_gfont' )){
 		add_option('bc_key_gfont', 'AIzaSyDU6NpGQZCsvykdP37jacEdRKmHpGSumEo');
 	}
         add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
